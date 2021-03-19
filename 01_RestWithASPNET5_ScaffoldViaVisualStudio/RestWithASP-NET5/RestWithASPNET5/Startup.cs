07fa87f9ea46bf618527da334ec18fdc06f1d525
anonimous2020 <maeshiro.michael@gmail.com>
2021-03-19 16:21:04 -0300

    implementação calculadora

--- 01_RestWithASPNET5_ScaffoldViaVisualStudio/RestWithASP-NET5/RestWithASPNET5/Startup.cs
+++ 01_RestWithASPNET5_ScaffoldViaVisualStudio/RestWithASP-NET5/RestWithASPNET5/Startup.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
-namespace RestWithASP_NET5
+namespace RestWithASPNET5
 {
     public class Startup
     {
@@ -25,23 +26,34 @@ namespace RestWithASP_NET5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNET5", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestWithASPNET5 v1"));
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
---
+++ 02_RestWithASPNET5_Calculadora/RestWithASP-NET5/RestWithASPNET5/Controllers/CalculadoraController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadoraController : ControllerBase
+    {   
+        private readonly ILogger<CalculadoraController> _logger;
+
+        public CalculadoraController(ILogger<CalculadoraController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("soma/{primeiroNumero}/{segundoNumero}")]
+        public IActionResult Get(string primeiroNumero, string segundoNumero)
+        {
+            if (IsNumeric(primeiroNumero) && IsNumeric(segundoNumero))
+            {
+                var sum = decimal.Parse(primeiroNumero) + decimal.Parse(segundoNumero);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("valor invalido");
+        }
+
+        private bool IsNumeric(string strNumero)
+        {
+            double numero;
+            bool isNumebr = double.TryParse(strNumero,System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out numero);
+            return isNumebr;
+        }
+    }
+}
